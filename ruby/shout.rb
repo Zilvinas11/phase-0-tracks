b0d70f2edb3b01f71d4c2304cd5830ad28357f0a
Zilvinas Peciura <z.peciura@gmail.com>
2016-09-10 13:43:13 -0500

    6.4 mix in a module,

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,40 @@
 # 6.4 writing a simple module
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-  def self.yelling_happily(word)
-   word + "!!!" + " :D"
-  end
+# module Shout
+#  def self.yell_angrily(words)
+ #   words + "!!!" + " :("
+  #end
+  #def self.yelling_happily(word)
+  # word + "!!!" + " :D"
+ # end
 
-end
+#end
+
 # mix in a module
+
+module Shout
+ def yell_angrily(words)
+   words + "!!!" + " :("
+  end
+  def yelling_happily(word)
+  word + "!!!" + " :D"
+  end
+end
+
+class Gril
+	include Shout
+end
+
+a=Gril.new
+
+p a.yelling_happily("hey")
+p a.yell_angrily("dndn")
+
+
+class Boy
+	include Shout
+end
+
+b=Boy.new
+
+p b.yell_angrily("hey")
+p b.yelling_happily("dkdkd")
